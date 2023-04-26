16a734003561e0420c7eb77ab937cb07bfb3ccbe
Lucas Santiago <lu.santi.oli@gmail.com>
2023-04-26 14:52:14 +0200

    fix: Wrong enum value

--- src/branch_type.c
+++ src/branch_type.c
@@ -17,11 +17,12 @@
 
 int is_internal_function(pid_t pid, struct user_regs_struct *regs)
 {
-    long res = ptrace(PTRACE_PEEKTEXT, pid, regs->rip, NULL);
+    long res = ptrace(PTRACE_PEEKTEXT, pid, regs->rax, NULL);
 
-    res &= 0xff;
-    if (res == 0xe8 || res == 0xff || res == 0x9a)
+    if (res == 0xe8 || res == 0xff || res == 0x9a) {
+        PRINT("FOUND INTERNAL CALL %ld\n", res);
         return 1;
+    }
     return 0;
 }
 
@@ -32,13 +33,13 @@ int handle_internal_function(pid_t pid, struct user_regs_struct *regs)
 
     if (next_instruction(pid) < 0)
         return -1;
-    if (ptrace(PTRACE_GETREGSET, pid, NULL, regs) < 0) {
+    if (ptrace(PTRACE_GETREGS, pid, NULL, regs) < 0) {
         perror("handle_internal_function ptrace error");
         return -1;
     }
     get_proc_info(&filepath_ptr, &address_ptr, pid, regs->rip);
     char *function_name = get_symbol_name(filepath_ptr, regs->rip);
-    PRINT("Entering function %s at 0x%lld", function_name, regs->rip);
+    PRINT("Entering function %s at 0x%lld\n", function_name, regs->rip);
     free(function_name);
     return 0;
 }
