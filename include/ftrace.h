fd3f9b0bc862c0d0df3f33b069d4accad9027242
Emil Pedersen <emil.pedersen@epitech.eu>
2023-04-27 14:30:14 +0200

    fix: false positives for call
    /2 and /3 were not checked for 0xff

--- include/ftrace.h
+++ include/ftrace.h
@@ -52,5 +52,5 @@ void get_proc_info(char **filepath_ptr, long *address_ptr,
 
 int ftrace(pid_t pid, struct settings *set);
 
-int is_internal_function(pid_t pid, struct user_regs_struct *regs);
+bool is_internal_function(pid_t pid, struct user_regs_struct *regs);
 int handle_internal_function(pid_t pid, struct user_regs_struct *regs);
