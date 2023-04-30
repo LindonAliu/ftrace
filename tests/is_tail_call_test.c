cd730413f8c060bb15dc2a4244400faf703847fe
Lindon ALIU <lindon.aliu@epitech.eu>
2023-04-30 18:09:07 +0200

    feat: add ff/5, ff/1 and ff/3 unit tests for is_tail_call.

--- tests/is_tail_call_test.c
+++ tests/is_tail_call_test.c
@@ -27,18 +27,39 @@ Test(is_tail_call, test_eb_opcode)
     cr_assert_eq(result, true);
 }
 
-Test(is_tail_call, test_ff_opcode_with_offset_2)
+Test(is_tail_call, test_ff_slash_4)
 {
     char bytes[8] = { 0xff, 0xe4, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00 };
     bool result = is_tail_call(*(long long *) bytes);
     cr_assert_eq(result, true);
 }
 
-Test(is_tail_call, test_ff_opcode_with_offset_3)
+Test(is_tail_call, test_ff_slash_4_2)
 {
     char bytes[8] = { 0xff, 0xe5, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00 };
     bool result = is_tail_call(*(long long *) bytes);
     cr_assert_eq(result, true);
+}
+
+Test(is_tail_call, test_ff_slash_5)
+{
+    char bytes[8] = { 0xff, 0xe5, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    bool result = is_tail_call(*(long long *) bytes);
+    cr_assert_eq(result, true);
+}
+
+Test(is_tail_call, test_ff_slash_1)
+{
+    char bytes[8] = { 0xff, 0xc1, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    bool result = is_tail_call(*(long long *) bytes);
+    cr_assert_eq(result, false);
+}
+
+Test(is_tail_call, test_ff_slash_3)
+{
+    char bytes[8] = { 0xff, 0xc3, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    bool result = is_tail_call(*(long long *) bytes);
+    cr_assert_eq(result, false);
 }
 
 Test(is_tail_call, test_invalid_opcode)
