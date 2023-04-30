f439f376c707857674e949c98d17ffd99d47a2fb
Emil Pedersen <emil.pedersen@epitech.eu>
2023-04-30 14:24:32 +0200

    test: add test for toto with printf

---
+++ tests/toto_printf.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2023
+** toto
+** File description:
+** FreeKOSOVO
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+void toto(void)
+{
+    printf("i am in toto()\n");
+}
+
+void tutu(void)
+{
+    printf("i am in tutu()\n");
+}
+
+int main(void)
+{
+    toto();
+    tutu();
+}
